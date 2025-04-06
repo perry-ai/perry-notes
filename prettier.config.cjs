eea593542d7d52436ec7e9a965cc53198b98fac0
perry-ai <perry-ai@outlook.com>
2025-04-07 00:17:47 +0800

    feat: add prettier configuration

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  // export default {
+  trailingComma: 'es5', // 在对象、数组等结构的最后一个元素后面添加逗号
+  tabWidth: 2, // 使用 2 个空格作为缩进
+  semi: false, // 不使用分号结束语句
+  singleQuote: true, // 使用单引号而不是双引号
+  printWidth: 120, // 每行代码的最大长度为 120 个字符
+  bracketSpacing: true, // 在对象字面量的括号之间添加空格
+  arrowParens: 'always', // 在箭头函数的参数周围始终使用括号，即使只有一个参数
+}
